e6448a11074a0c1f7f1153de8912abe4c3df0ba2
keerthi-ar <keerthianandreddy@gmail.com>
2024-09-13 13:20:37 +0530

    Update model_evaluation.py

--- src/Text_Summarizer/components/model_evaluation.py
+++ src/Text_Summarizer/components/model_evaluation.py
@@ -5,19 +5,13 @@ import pandas as pd
 from tqdm import tqdm
 from Text_Summarizer.entity import ModelEvaluationConfig
 
-
-
-
 class ModelEvaluation:
     def __init__(self, config: ModelEvaluationConfig):
         self.config = config
 
-
-    
     def generate_batch_sized_chunks(self,list_of_elements, batch_size):
         for i in range(0, len(list_of_elements), batch_size):
             yield list_of_elements[i : i + batch_size]
-
     
     def calculate_metric_on_test_ds(self,dataset, metric, model, tokenizer, 
                                batch_size=16, device="cuda" if torch.cuda.is_available() else "cpu", 
@@ -36,16 +30,16 @@ class ModelEvaluation:
                             attention_mask=inputs["attention_mask"].to(device), 
                             length_penalty=0.8, num_beams=8, max_length=128)
             
-            decoded_summaries = [tokenizer.decode(s, skip_special_tokens=True,clean_up_tokenization_spaces=True) s in summaries]      
+            decoded_summaries = [tokenizer.decode(s, skip_special_tokens=True, 
+                                    clean_up_tokenization_spaces=True) 
+                for s in summaries]      
             
             decoded_summaries = [d.replace("", " ") for d in decoded_summaries]
-            
             
             metric.add_batch(predictions=decoded_summaries, references=target_batch)
             
         score = metric.compute()
         return score
-
 
     def evaluate(self):
         device = "cuda" if torch.cuda.is_available() else "cpu"
